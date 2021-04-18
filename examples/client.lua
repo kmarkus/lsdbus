3bfe100bbe9e9e636188f51e2a6c26f81a150085
Markus Klotzbuecher <mk@mkio.de>
2021-04-18 23:01:16 +0200

    add examples
    Signed-off-by: Markus Klotzbuecher <mk@mkio.de>

---
+++ examples/client.lua
@@ -0,0 +1,16 @@
+local u = require("utils")
+local lsdb = require("lsdbus")
+local b = lsdb.open()
+
+print("UPower")
+u.pp(b:call('org.freedesktop.UPower',
+	    '/org/freedesktop/UPower',
+	    'org.freedesktop.DBus.Introspectable',
+	    'Introspect'))
+
+print("timedata1")
+u.pp(b:call('org.freedesktop.timedate1',
+	    '/org/freedesktop/timedate1',
+	    'org.freedesktop.timedate1',
+	    'ListTimezones',
+	    ""))
