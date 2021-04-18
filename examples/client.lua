4a209816b27b1f2a0996d91d6e3014c9a345d5c9
Markus Klotzbuecher <mk@mkio.de>
2021-04-18 23:01:16 +0200

    add more examples
    Signed-off-by: Markus Klotzbuecher <mk@mkio.de>

--- examples/client.lua
+++ examples/client.lua
@@ -14,3 +14,23 @@ u.pp({b:call('org.freedesktop.timedate1',
 	     'org.freedesktop.timedate1',
 	     'ListTimezones',
 	     "")})
+
+print("bluez hci0 'Discovering' property")
+local r, res, res2 = b:call('org.bluez',
+			   '/org/bluez/hci0',
+			   'org.freedesktop.DBus.Properties',
+			   'Get',
+			   "ss",
+			   "org.bluez.Adapter1", "Discovering")
+
+print(r, u.tab2str(res), res2)
+
+print("bluez hci0 GetAll properties")
+local r, res = b:call('org.bluez',
+		      '/org/bluez/hci0',
+		      'org.freedesktop.DBus.Properties',
+		      'GetAll',
+		      "s",
+		      "org.bluez.Adapter1")
+
+print(r, u.tab2str(res))
