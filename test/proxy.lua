4f02661340bca992ae3eb50c9599136f97de7989
Markus Klotzbuecher <mk@mkio.de>
2021-04-26 20:06:38 +0200

    proxy: add tests
    Signed-off-by: Markus Klotzbuecher <mk@mkio.de>

---
+++ test/proxy.lua
@@ -0,0 +1,48 @@
+local lu=require("luaunit")
+local lsdb = require("lsdbus")
+local proxy = require("lsdbus_proxy")
+
+local timedate1_if = {
+   name = 'org.freedesktop.timedate1',
+
+   methods = {
+      ListTimezones = {},
+   },
+
+   properties = {
+      CanNTP = { type='boolean', access='read' },
+      NTP = { type='boolean', access='read' },
+      Timezone = { type='string', access='read' },
+   }
+
+}
+
+local hostname1_if = {
+   name = 'org.freedesktop.hostname1',
+
+   methods = {
+   },
+
+   properties = {
+      Chassis = { type='string', access='read' },
+   }
+}
+
+TestProxy = {}
+
+local b, td, hn
+
+function TestProxy:setup()
+   b = lsdb.open('default_system')
+
+   td = proxy:new(b, 'org.freedesktop.timedate1', '/org/freedesktop/timedate1', timedate1_if)
+   hn = proxy:new(b, 'org.freedesktop.hostname1', '/org/freedesktop/hostname1', hostname1_if)
+end
+
+function TestProxy:TestSimpleCall()
+   lu.assertIsTable(td('ListTimezones'))
+   lu.assertEquals(td.Timezone, "Europe/Berlin")
+   lu.assertEquals(hn.Chassis, "laptop")
+end
+
+os.exit( lu.LuaUnit.run() )
