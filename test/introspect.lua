2996451bed75aba38191c8f65f4b1e96155b2800
Markus Klotzbuecher <mk@mkio.de>
2021-05-15 23:34:16 +0200

    tests: add minimal introspection test
    Signed-off-by: Markus Klotzbuecher <mk@mkio.de>

--- test/introspect.lua
+++ test/introspect.lua
@@ -3,10 +3,33 @@ local lsdb = require("lsdbus")
 
 local TestIntrospect = {}
 
+local testnode = {
+   {
+      name="com.example.SampleInterface0",
+      methods={
+	 Bazify={
+	    {direction="in", name="bar", type="(iiu)"},
+	    {direction="out", name="bar", type="v"}
+	 },
+	 Frobate={
+	    {direction="in", name="foo", type="i"},
+	    {direction="out", name="bar", type="s"},
+	    {direction="out", name="baz", type="a{us}"}
+	 },
+	 Mogrify={{direction="in", name="bar", type="(iiav)"}}
+      },
+
+      properties={Bar={access="readwrite", type="y"}},
+      signals={Changed={{name="new_value", type="b"}}}
+   }
+}
+
+
 function TestIntrospect:TestReadXML()
-   local f = "/usr/share/dbus-1/interfaces/org.freedesktop.PackageKit.xml"
-   print("parsing ", f)
-   lsdb.xml_read(f)
+   local f = "testnode.xml"
+   local node = lsdb.xml_fromfile(f)
+   lu.assertIsTable(node)
+   lu.assertEquals(testnode, node)
 end
 
 return TestIntrospect
--- test/proxy.lua
+++ test/proxy.lua
@@ -2,32 +2,6 @@ local lu=require("luaunit")
 local lsdb = require("lsdbus")
 local proxy = require("lsdbus_proxy")
 
-local timedate1_if = {
-   name = 'org.freedesktop.timedate1',
-
-   methods = {
-      ListTimezones = {},
-   },
-
-   properties = {
-      CanNTP = { type='boolean', access='read' },
-      NTP = { type='boolean', access='read' },
-      Timezone = { type='string', access='read' },
-   }
-
-}
-
-local hostname1_if = {
-   name = 'org.freedesktop.hostname1',
-
-   methods = {
-   },
-
-   properties = {
-      Chassis = { type='string', access='read' },
-   }
-}
-
 TestProxy = {}
 
 local b, td, hn
@@ -35,8 +9,8 @@ local b, td, hn
 function TestProxy:setup()
    b = lsdb.open('default_system')
 
-   td = proxy:new(b, 'org.freedesktop.timedate1', '/org/freedesktop/timedate1', timedate1_if)
-   hn = proxy:new(b, 'org.freedesktop.hostname1', '/org/freedesktop/hostname1', hostname1_if)
+   td = proxy:new(b, 'org.freedesktop.timedate1', '/org/freedesktop/timedate1', 'org.freedesktop.timedate1')
+   hn = proxy:new(b, 'org.freedesktop.hostname1', '/org/freedesktop/hostname1', 'org.freedesktop.hostname1')
 end
 
 function TestProxy:TestPing()
