56fa9bd2a97820c567280049925b1618069949a9
Markus Klotzbuecher <mk@mkio.de>
2022-04-29 19:54:19 +0200

    tools: add lsdb-call, lsdb-mon and lsdb-emit
    Signed-off-by: Markus Klotzbuecher <mk@mkio.de>

--- examples/mon-signal.lua
+++ examples/mon-signal.lua
@@ -7,6 +7,14 @@ local function exit(sig)
    b:exit_loop()
 end
 
+local function safe(func, log)
+   return function(...)
+      local res, ret = pcall(func, ...)
+      if not res then log(ret); return 1 end
+      return ret
+   end
+end
+
 b:add_signal("SIGINT", exit)
-b:match_signal(nil, nil, nil, nil, u.pp)
+b:match_signal(nil, nil, nil, nil, safe(function() error("something failed") end, print))
 b:loop()
