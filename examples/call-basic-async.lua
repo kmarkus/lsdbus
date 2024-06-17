7db679654b4057ff903fd9cd7b6feeb75a09c453
Markus Klotzbuecher <mk@mkio.de>
2024-06-17 22:17:26 +0200

    examples: update call-basic-async
    Signed-off-by: Markus Klotzbuecher <mk@mkio.de>

--- examples/call-basic-async.lua
+++ examples/call-basic-async.lua
@@ -1,45 +1,39 @@
 local u = require("utils")
 local lsdb = require("lsdbus.core")
 
-local callback
-local slot
-local loop = 100000*2
+local slots = {}
 
-local function call_async1(b)
-   return b:call_async(callback,
+local function call_async1(b, i)
+   local function cb1(...)
+      u.pp("callback1", i, ...)
+      slots[i] = nil
+   end
+   return b:call_async(cb1,
 		       'org.freedesktop.UPower',
 		       '/org/freedesktop/UPower',
 		       'org.freedesktop.DBus.Introspectable',
 		       'Introspect')
 end
 
-local function call_async2(b)
-   return b:call_async(callback,
+local function call_async2(b, i)
+   local function cb2(...)
+      u.pp("callback2", i, ...)
+      slots[i] = nil
+   end
+   return b:call_async(cb2,
 		       'org.freedesktop.timedate1',
 		       '/org/freedesktop/timedate1',
 		       'org.freedesktop.timedate1',
 		       'ListTimezones', "")
 end
 
-function callback(b, ...)
-   if loop<0 then print("exiting loop"); b:exit_loop() end
-   slot = call_async1(b)
-   loop = loop - 1
-   collectgarbage('collect')
-   print(slot, loop, collectgarbage('count'))
-end
-
 local b = lsdb.open('default_system')
-slot = call_async2(b)
 
--- throws EEXIST, it seems only one async call is allowed simultaneously.
+for i=1,100,2 do
+   slots[i] = call_async1(b, i)
+   slots[i+1] = call_async2(b, i+1)
 
--- u.pp(b:call_async(
--- 	callback,
--- 	'org.freedesktop.timedate1',
---  	'/org/freedesktop/timedate1',
---  	'org.freedesktop.timedate1',
---  	'ListTimezones', ""))
-
+   while b:run() ~= 0 do end
+end
 
 b:loop()
