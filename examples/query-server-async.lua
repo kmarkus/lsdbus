5697e73f622ceb0df27f67e316564d7398697443
Markus Klotzbuecher <mk@mkio.de>
2024-06-17 20:46:31 +0200

    examples: update query async example
    Signed-off-by: Markus Klotzbuecher <mk@mkio.de>

--- examples/query-server-async.lua
+++ examples/query-server-async.lua
@@ -3,31 +3,31 @@ local lsdb = require("lsdbus")
 
 local tst
 local cnt = 0
-local pow_cb=nil
 
 local function concat_cb(b, ...)
-   u.pp("callback", b:context(), ...)
-   u.pp(tst:call_async('pow', pow_cb, cnt))
+   u.pp("concat callback", b:context(), ...)
    cnt = cnt + 1
 end
 
-function pow_cb(b, ...)
-   u.pp("callback", b:context(), ...)
-   u.pp(tst:call_async('concat', concat_cb, 'a', 'b', tostring(cnt)))
+local function pow_cb(b, ...)
+   u.pp("pow callback", b:context(), ...)
 end
-
 
 local function fail_cb(b, ...)
    u.pp("fail_cb", b:context(), {...})
-   u.pp(tst:call_async('FailWithDBusError', fail_cb))
 end
 
 local b = lsdb.open('user')
 tst = lsdb.proxy.new(b, 'lsdbus.test', '/', 'lsdbus.test.testintf0')
 
-u.pp(tst:call_async('pow', pow_cb, 0))
---u.pp(tst:call_async('Fail', fail_cb))
---u.pp(tst:call_async('FailWithDBusError', fail_cb))
+local s1,s2,s3,s4,s5,s6
 
-
-b:loop()
+while true do
+   s1 = tst:call_async('pow', pow_cb, cnt)
+   s2 = tst:call_async('concat', concat_cb, 'a', 'b', tostring(cnt))
+   s3 = tst:call_async('thunk', function () u.pp("thunk cb") end)
+   s4 = tst:call_async('twoin', function (_, ...) u.pp("twoin cb", ...) end, cnt, {"a","b"})
+   s5 = tst:call_async('twoout', function (_, ...) u.pp("twoout cb", ...) end)
+   s6 = tst:call_async('Fail', fail_cb)
+   while b:run(100) ~= 0 do end
+end
