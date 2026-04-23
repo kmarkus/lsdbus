8ef9823de78a048379deda490377dbfb51fce607
Francois Perrad <francois.perrad@gadz.org>
2026-04-23 21:46:55 +0200

    lsdbus.expat: D-Bus XML parser using luaexpat (lxp.lom)
    Based on original work by Francois Perrad.
    
    Originally-by: Francois Perrad <francois.perrad@gadz.org>

---
+++ src/lsdbus/expat.lua
@@ -0,0 +1,76 @@
+-- lsdbus.expat: D-Bus introspection XML parser using luaexpat (lxp.lom).
+-- Loaded by lsdbus when the mxml C backend is not compiled in.
+-- Contributed by Francois Perrad (fperrad/expat branch).
+
+local lom = require("lxp.lom")
+
+local M = {}
+
+local function lom2node(doc)
+   local interfaces = {}
+   local nodes = {}
+   for _, elt1 in ipairs(doc) do
+      if type(elt1) == 'table' then
+         if elt1.tag == 'interface' then
+            local methods = {}
+            local properties = {}
+            local signals = {}
+            for _, elt2 in ipairs(elt1) do
+               if type(elt2) == 'table' then
+                  if elt2.tag == 'method' then
+                     local args = {}
+                     for _, elt3 in ipairs(elt2) do
+                        if type(elt3) == 'table' and elt3.tag == 'arg' then
+                           args[#args+1] = {
+                              name      = elt3.attr.name,
+                              type      = elt3.attr.type,
+                              direction = elt3.attr.direction,
+                           }
+                        end
+                     end
+                     methods[elt2.attr.name] = args
+                  elseif elt2.tag == 'property' then
+                     properties[elt2.attr.name] = {
+                        access = elt2.attr.access,
+                        type   = elt2.attr.type,
+                     }
+                  elseif elt2.tag == 'signal' then
+                     local args = {}
+                     for _, elt3 in ipairs(elt2) do
+                        if type(elt3) == 'table' and elt3.tag == 'arg' then
+                           args[#args+1] = {
+                              name = elt3.attr.name,
+                              type = elt3.attr.type,
+                           }
+                        end
+                     end
+                     signals[elt2.attr.name] = args
+                  end
+               end
+            end
+            interfaces[#interfaces+1] = {
+               name       = elt1.attr.name,
+               methods    = methods,
+               properties = properties,
+               signals    = signals,
+            }
+         elseif elt1.tag == 'node' then
+            nodes[#nodes+1] = elt1.attr.name
+         end
+      end
+   end
+   return { name=doc.attr.name, interfaces=interfaces, nodes=nodes }
+end
+
+function M.xml_fromfile(filename)
+   local f = assert(io.open(filename, 'r'))
+   local xml = f:read("*a")
+   f:close()
+   return lom2node(assert(lom.parse(xml)))
+end
+
+function M.xml_fromstr(str)
+   return lom2node(assert(lom.parse(str)))
+end
+
+return M
